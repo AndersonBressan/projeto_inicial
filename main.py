d4ec1f6a617abe77c8b42d2e923faba19dd144c8
Anderson Lima Bressan <andersonlimabressanalb@gmail.com>
2024-09-25 10:51:15 -0300

    Salvando o arquivo main

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Jornada de dados")
+
+# Declaraçã de Variáveis
+
+idade = 37
+nome = 'Anderson'
+
+print(idade)
+print(nome)
+
+#Lista
+lista = [1,2,3,4,5]
+print(lista)
+
+# metodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
